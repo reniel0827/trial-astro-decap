27360d10494f5eb7cc3cf5f08872d2fed86aa755
reniel0827 <renielvillegas@gmail.com>
2025-03-24 21:53:42 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: "https://www.yourwebsite.com", // update me!
+  site: "trial-astro-blog.netlify.app", // update me!
   integrations: [
     icon(),
     sitemap({
